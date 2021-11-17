032f3c68c456d5d3e3c5884a93b845a3ede258f6
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-11-17 22:18:21 +0100

    manipulate array

--- day1_151121/manipulate_array.js
+++ day1_151121/manipulate_array.js
@@ -1,5 +1,39 @@
+/*
 const myArray = Array();
+
 console.log(myArray);
+*/
+
+/*
 
 const emptyValues = Array(8);
-console.log(emptyValues);
+console.log(emptyValues.length);
+*/
+
+/*
+const eightValues = Array(8).fill("EFREI");
+console.log(eightValues);
+*/
+
+/*
+const fruits = ['banana', 'orange', 'mango', 'lemon'];
+let index = fruits.indexOf("banana");
+
+if(index != -1) {
+    console.log("Cet fruits est disponible dans notre magasin!");
+}
+else {
+    console.log("Cet fruits n'est pas disponible.");
+}
+*/
+
+/*
+const fruits = ['banana', 'orange', 'mango', 'lemon'];
+let result = fruits.indexOf("tomate");
+
+(result != -1) ? console.log('Tomate est disponible') : console.log('Tomate n\'est pas disponible dans mgs');
+*/
+
+const myArray = ["tomate", "kaki", "orange", "lemon"];
+let indexFruit = myArray.indexOf("orange");
+console.log(indexFruit);
