d5557a7d9f32bf92851bdb6fc3e3c6a7d520a630
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-11-22 23:33:33 +0100

    add index and app js

---
+++ day3_181121/app.js
@@ -0,0 +1,11 @@
+/*
+const header = (
+    <div>
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+        <p>Asabeneh Yetayeh</p>
+        <small>18 novembre 2021</small>
+    </div>
+)
+*/
