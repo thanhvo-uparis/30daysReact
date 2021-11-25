f025f4e672d01d97ad709d69afab5f289c321fe8
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-11-25 14:19:35 +0100

    complete exercice 1

--- day3/src/index.js
+++ day3/src/index.js
@@ -10,21 +10,37 @@ const application = (
   </div>
 )
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import htmlLogo from './html_logo.png'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import htmlLogo from './assets/img/html_logo.png'
+import cssLogo from './assets/img/css_logo.png'
+import jsLogo from './assets/img/js_logo.png'
+import reactLogo from './assets/img/reactjs_logo.png'
+import css from './index.css'
 
 const heading = "Front End Technologies"
 const html = (
-    <div>
-        <img src={htmlLogo} ></img>
+    <div className="header-container" >
+        <div className="header-list">
+          <div className="list-items">
+              <img src={htmlLogo} ></img>
+          </div>
+          <div className="list-items">
+              <img src={cssLogo} ></img>
+          </div>
+          <div className="list-items">
+              <img src={jsLogo} ></img>
+          </div>
+          <div className="list-items-reactjs">
+              <img src={reactLogo} ></img>
+          </div>
+        </div>
     </div>
 )
 
 const application = (
-  <div>
-    <p>{heading}</p> 
+  <div className="container">
+    <p className="heading">{heading}</p> 
     {html}
   </div>
 )
